18b8150da112e22dd67473585a2a24552e312bde
joaoUzelotto <uzelottojoao@gmail.com>
2025-08-05 10:33:40 -0400

    5-08-2025

---
+++ aula2/aula2.py
@@ -0,0 +1,10 @@
+nome1 = "João"
+nome2 = "Victor"
+
+tamanhonome1 = len(nome1)
+tamanhonome2 = len(nome2)
+
+if tamanhonome1 > tamanhonome2:
+    print("João é maior.")
+elif tamanhonome2 > tamanhonome1:
+    print("Victor é maior.")
